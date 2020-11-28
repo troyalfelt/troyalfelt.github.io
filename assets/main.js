04db6cbbf81a8673dde3400243dbf7b238771e48
Troy Alfelt <troyalfelt@gmail.com>
2020-11-28 15:27:42 -0500

    ran prettier to fix formatting

--- assets/main.js
+++ assets/main.js
@@ -5,35 +5,35 @@ let random = document.querySelector(".random");
 const change = function () {
   let num = Math.floor(Math.random() * 10);
   if (num == 0) {
-  accentDiv1.style.backgroundColor = "pink";
-  accentDiv2.style.backgroundColor = "pink";
-} else if (num == 1) {
-  accentDiv1.style.backgroundColor = "green";
-  accentDiv2.style.backgroundColor = "green";
-} else if (num == 2) {
-  accentDiv1.style.backgroundColor = "black";
-  accentDiv2.style.backgroundColor = "black";
-} else if (num == 3) {
-  accentDiv1.style.backgroundColor = "white";
-  accentDiv2.style.backgroundColor = "white";
-} else if (num == 4) {
-  accentDiv1.style.backgroundColor = "orange";
-  accentDiv2.style.backgroundColor = "orange";
-} else if (num == 5) {
-  accentDiv1.style.backgroundColor = "purple";
-  accentDiv2.style.backgroundColor = "purple";
-} else if (num == 6) {
-  accentDiv1.style.backgroundColor = "gray";
-  accentDiv2.style.backgroundColor = "gray";
-} else if (num == 7) {
-  accentDiv1.style.backgroundColor = "red";
-  accentDiv2.style.backgroundColor = "red";
-} else if (num == 8) {
-  accentDiv1.style.backgroundColor = "brown";
-  accentDiv2.style.backgroundColor = "brown";
-} else if (num == 9) {
-  accentDiv1.style.backgroundColor = "yellow";
-  accentDiv2.style.backgroundColor = "yellow";
-}
+    accentDiv1.style.backgroundColor = "pink";
+    accentDiv2.style.backgroundColor = "pink";
+  } else if (num == 1) {
+    accentDiv1.style.backgroundColor = "green";
+    accentDiv2.style.backgroundColor = "green";
+  } else if (num == 2) {
+    accentDiv1.style.backgroundColor = "black";
+    accentDiv2.style.backgroundColor = "black";
+  } else if (num == 3) {
+    accentDiv1.style.backgroundColor = "white";
+    accentDiv2.style.backgroundColor = "white";
+  } else if (num == 4) {
+    accentDiv1.style.backgroundColor = "orange";
+    accentDiv2.style.backgroundColor = "orange";
+  } else if (num == 5) {
+    accentDiv1.style.backgroundColor = "purple";
+    accentDiv2.style.backgroundColor = "purple";
+  } else if (num == 6) {
+    accentDiv1.style.backgroundColor = "gray";
+    accentDiv2.style.backgroundColor = "gray";
+  } else if (num == 7) {
+    accentDiv1.style.backgroundColor = "red";
+    accentDiv2.style.backgroundColor = "red";
+  } else if (num == 8) {
+    accentDiv1.style.backgroundColor = "brown";
+    accentDiv2.style.backgroundColor = "brown";
+  } else if (num == 9) {
+    accentDiv1.style.backgroundColor = "yellow";
+    accentDiv2.style.backgroundColor = "yellow";
+  }
 };
 random.addEventListener("click", change);
